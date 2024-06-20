95ef1f1f3a88185e5a310d6be49a1852d77b9c2e
Jojo-star7 <jojonabu49@gmail.com>
2024-06-20 06:11:00 +0300

    update the search engine functionality using API

--- src/app.js
+++ src/app.js
@@ -1,9 +1,26 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let cityElement = document.querySelector("#city");
+  temperature = response.data.temperature.current;
+
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
+function searchCity(city) {
+  let apikey = "o0a84d23edbd856fet0dd8ae716ab462";
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apikey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
+}
+
 function handleSearchSubmit(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#search-form-input");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.input;
+
+  searchCity(searchInput.value);
 }
 
 let searchFormElement = document.querySelector("#search-form");
 searchFormElement.addEventListener("submit", handleSearchSubmit);
+
+searchCity("Paris");
